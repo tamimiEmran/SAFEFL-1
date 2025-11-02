75dcda5e6785cb5d37962fa7856ca402bfc60824
tamimiEmran <tamimiemran@gmail.com>
2025-11-02 05:11:14 +0300

    Update run_benchmarks.py

--- run_benchmarks.py
+++ run_benchmarks.py
@@ -22,7 +22,7 @@ nbyz_list = [25, 50]
 
 base_args = [
     "--nworkers", "200",
-    "--batch_size", "128",
+    "--batch_size", "256",
     "--niter", "2500", #2500
     "--lr", "0.05",
     "--test_every", "10", #10
