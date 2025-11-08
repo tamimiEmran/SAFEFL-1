ffc7fcc991e9c50cb16a256fef5bd0f9cf8f1207
tamimiEmran <tamimiemran@gmail.com>
2025-11-08 17:42:26 +0300

    Update run_benchmarks.py

--- run_benchmarks.py
+++ run_benchmarks.py
@@ -9,23 +9,23 @@ import shlex
 
 # customize these lists as needed
 #datasets = ["FEMNIST", 'MNIST', 'CIFAR10']
-datasets = ['FEMNIST','MNIST'] # for debugging purposes, only run MNIST
-bias_values = [0, 0.5] # for debugging purposes, only run 0
+datasets = ['MNIST', 'FEMNIST'] # for debugging purposes, only run MNIST
+bias_values = [0,0.25, 0.5] # for debugging purposes, only run 0
 #models = ["resnet18", "mobilenet_v3_small", "vit_base"] # Only keep resnet18
-models = ["resnet18"]
+models = ["mobilenet_v3_small", "eff_net"]
 attack_types = ["no", "scaling_attack", "label_flipping_attack"]
 #full_list_defences = ["fedavg", "krum", "trim_mean", "median", "flame", "shieldfl", "divide_and_conquer", "signguard", "flare", "romoa"] # Keep only 
-defences = ['fedavg', 'krum', 'shieldfl', 'signguard', 'FactorGraphs']
+defences = ['fedavg', 'krum', 'shieldfl', 'signguard']
 isGrouped_list = [True, False]
-group_size_list = [10, 20] 
-nbyz_list = [25, 50] 
+group_size_list = [25, 50] 
+nbyz_list = [50] 
 
 base_args = [
-    "--nworkers", "200",
+    "--nworkers", "500",
     "--batch_size", "256",
-    "--niter", "2500", #2500
+    "--niter", "3000", #2500
     "--lr", "0.05",
-    "--test_every", "10", #10
+    "--test_every", "30", #10
     "--gpu", "1",
 ]
 
